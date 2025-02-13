6aff156e01e77f44fc4224a38fdb7b9a5b454c38
Daniel Rubery <drubery@google.com>
2025-02-13 12:55:48 -0800

    Update deploy.sh to include multiple remotes and fix remixes (#100)
    In order to streamline development, we want people to be able to use a
    remixed server for staging changes. Creating a remix currently requires
    modifying .env each time. This commit fixes than and updates deploy.sh
    to support a staging remote.
    
    Co-authored-by: Daniel Rubery <drubery@chromium.org>

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,39 @@
 set -e
 
 # Usage:
-#   ./deploy.sh <branch to deploy>
-# The branch is optional and defaults to origin/glitch.
-#
-# This requires you to have an upstream called "glitch" which points to
-# the URL fetched from the Glitch UI. You can create one with:
-#   git remote add glitch $GLITCH_URL
+#   ./deploy.sh <remote name> <branch name>
+# Pushes the current state of <branch name> to the Glitch remote <remote
+# name>. <branch name> defaults to "origin/glitch", and <remote name> to
+# "glitch".
 
-git new-branch --upstream glitch/main deploy_glitch
-git checkout ${1:-origin/glitch} -- .
+# One time setup:
+
+# This requires you to have setup an upstream which points to
+# the URL fetched from the Glitch UI. You can create one with:
+#   git remote add <remote name> $GLITCH_URL
+#   git pull <remote name>
+# It is recommended to have one upstream called "glitch" for the
+# prototype server, https://glitch.com/edit/#!/dbsc-prototype-server,
+# and one for a personal remix of that project you can use for staging.
+
+# Recommended Development process:
+
+# Create a new branch tracking the main repo:
+#   git new-branch --upstream origin/glitch <branch name>
+# Develop your code on that branch
+# Test with:
+#   ./deploy.sh <personal remix> <branch name>
+# When satisfied, push this branch and create a pull request:
+#   git push -u origin <branch name>
+# When the pull request is landed, deploy to the prototype server:
+#   ./deploy.sh
+
+BRANCH=${2:-origin/glitch}
+REMOTE=${1:-glitch}
+
+git new-branch --upstream $REMOTE/main deploy_glitch
+git checkout $BRANCH -- .
 git commit -a -m "Automated deployment"
-git push -u glitch deploy_glitch:main
-git checkout ${1:-origin/glitch}
+git push -u $REMOTE deploy_glitch:main
+git checkout $BRANCH
 git branch -D deploy_glitch
